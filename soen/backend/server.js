ac4276881abae950c37cbfffde9f4504c79957a6
Bubble Wrap <vishalpandey000090@gmail.com>
2026-08-15 00:19:08 +0530

    Add CORP header for Service Worker

--- soen/backend/server.js
+++ soen/backend/server.js
@@ -49,6 +49,12 @@ app.use(cors({
     ],
     credentials: true
 }));
+
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
